c565ed8d2d5ca835ffe5e31f0b6fcb81795e4ef8
micah <1569124799@qq.com>
2023-04-09 14:39:33 +0800

    新注册用户随机头像

--- goods_example/src/main/java/com/micah/goods_example/service/impl/UserServiceImpl.java
+++ goods_example/src/main/java/com/micah/goods_example/service/impl/UserServiceImpl.java
@@ -11,12 +11,8 @@ import com.micah.goods_example.entity.model.RegisterParam;
 import com.micah.goods_example.enums.AppFromEnum;
 import com.micah.goods_example.enums.AppHttpCodeEnum;
 import com.micah.goods_example.service.UserService;
-import com.micah.goods_example.util.BeanCopyUtils;
-import com.micah.goods_example.util.JwtUtil;
-import com.micah.goods_example.util.R;
-import com.micah.goods_example.util.ResponseResult;
+import com.micah.goods_example.util.*;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -72,7 +68,7 @@ public class UserServiceImpl extends ServiceImpl<UserDao, User> implements UserS
                 .setUpdateTime(new Date())
                 .setIsDel(Boolean.FALSE)
                 .setCreateTime(new Date())
-                .setAvator("https://th.bing.com/th/id/R.629105f8e48009c659b8a4048adef1cc?rik=0LttQdHqnl3ang&riu=http%3a%2f%2fimg.wxcha.com%2ffile%2f202005%2f19%2fdeb4d01b77.jpg&ehk=6DKzNVR5O4Oa%2fZxvhM9ea0W9jpJVSfAZVW4mPemcz3k%3d&risl=&pid=ImgRaw&r=0")
+                .setAvator("https://api.multiavatar.com/"+ StringUtils.generateRandomLetters(5) +".png")
                 .setEmail(registerParam.getEmail());
 
         boolean save = save(user);
---
+++ goods_example/src/main/java/com/micah/goods_example/util/StringUtils.java
@@ -0,0 +1,24 @@
+package com.micah.goods_example.util;
+
+import java.util.Random;
+
+/**
+ * @ClassName StringUtils
+ * @Description 简介
+ * @Author Micah
+ * @Date 2023/4/9 14:37
+ * @Version 1.0
+ **/
+public class StringUtils {
+
+    public static String generateRandomLetters(int length) {
+        Random random = new Random();
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(alphabet.length());
+            sb.append(alphabet.charAt(index));
+        }
+        return sb.toString();
+    }
+}
